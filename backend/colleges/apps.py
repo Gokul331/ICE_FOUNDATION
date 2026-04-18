5b5037d261355fa1f777e9063da04776f6ec5398
gokul331 <gokulece303@gmail.com>
2026-04-18 20:26:44 +0530

    Change the app name as colleges

--- backend/colleges/apps.py
+++ backend/colleges/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class CollegeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'college'
+    name = 'colleges'
 
     def ready(self):
         from .signals import create_admin
