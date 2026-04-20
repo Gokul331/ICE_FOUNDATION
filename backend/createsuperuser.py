8d26093ae57e39fddfcdec094ad6d27fe28a30b0
gokul331 <gokulece303@gmail.com>
2026-04-20 15:22:44 +0530

    Add user

--- backend/createsuperuser.py
+++ backend/createsuperuser.py
@@ -1,24 +1,18 @@
 import os
 from django.contrib.auth import get_user_model
 
-print("🔥 Running superuser script...")
-
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@gmail.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
 
-print(username, email, password)
+user, created = User.objects.get_or_create(username=username)
 
-if username and email and password:
-    user, created = User.objects.get_or_create(username=username)
-    user.email = email
-    user.set_password(password)
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
+user.email = email
+user.set_password(password)
+user.is_staff = True
+user.is_superuser = True
+user.save()
 
-    print("✅ Superuser created/updated")
-else:
-    print("❌ Missing environment variables")
+print("Superuser ready")
