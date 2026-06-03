bef877a05de7ec8c0c592d4b7ed41dc7f70a29c3
gokul331 <gokulece303@gmail.com>
2026-06-03 10:14:12 +0530

    adding the superuser password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -328,7 +328,7 @@ if not DEBUG and os.environ.get('DATABASE_URL') and os.environ.get('DJANGO_SUPER
         
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
         
         if password and not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
